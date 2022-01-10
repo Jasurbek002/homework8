a0134faaf903ba3a2c347516fb145546ff2b3587
unknown <jasurbekturgunov25@gmail.com>
2022-01-10 13:02:19 +0500

    yangilandai

--- js/main.js
+++ js/main.js
@@ -14,26 +14,57 @@ var isvelosaped = document.getElementById('velosaped')
 var ismashina = document.getElementById('mashina')
 var issamalyot = document.getElementById('samalyot')
 
-form.addEventListener('submit', event =>{
+// form.addEventListener('submit', event =>{
+//     event.preventDefault()
+//     console.log(isinput.value)
+    
+//     ispiyoda.textContent = getTime (piyoda_v, isinput.value)
+//     isvelosaped.textContent = getTime (velosaped_v, isinput.value)
+//     ismashina.textContent = getTime (mashina_v, isinput.value)
+//     issamalyot.textContent = getTime (samalyot_v,isinput.value)
+
+// })
+//     function getTime (speed,distance) {
+//         let time = distance/speed
+
+//         return nromalizeTime(time)
+
+//     }
+
+//     function nromalizeTime(time){
+//         let hour = Math.floor(time/ 60)
+//         let minute = Math.round((time-hour) *60)
+
+//         return `${hour} soat  ${minute} minut`
+//     }
+
+// biz html dan chaqirilgan formni addEvevntlistiner yordamida kuzatib uni 'submit' bo'lgan paytida javascriptga
+// buyurilgan ishni bajarishini aytamiz. bizga [speed]-tezlik va [distance]-masofa kirib keladi isinput orqali
+// biz function yordamida bu amalni bajaramiz [speed]/[distance] bu funksiya bizga faqat soatni chiqarib berad 
+// biz yana bir function yaratamiz bu function bizga N soat N minut deb hsoblab beradi va bundan qaytagan returnni
+// birinchi functionga birichisini nomini form ichidaga isinputga beramiz
+
+
+form.addEventListener('submit', event=>{
     event.preventDefault()
     console.log(isinput.value)
-    
-    ispiyoda.textContent = getTime (piyoda_v, isinput.value)
-    isvelosaped.textContent = getTime (velosaped_v, isinput.value)
-    ismashina.textContent = getTime (mashina_v, isinput.value)
-    issamalyot.textContent = getTime (samalyot_v,isinput.value)
+
+    ispiyoda.textContent=getwell(piyoda_v,isinput.value)
+        isvelosaped.textContent=getwell(velosaped_v,isinput.value)
+    ismashina.textContent=getwell(mashina_v,isinput.value)
+    issamalyot.textContent=getwell(samalyot_v,isinput.value)
+
 
 })
-    function getTime (speed,distance) {
-        let time = distance/speed
 
-        return nromalizeTime(time)
+function getwell(tezlik,masofa){
+    let vaqt =masofa/tezlik
 
-    }
+    return valution(vaqt)
+}
 
-    function nromalizeTime(time){
-        let hour = Math.floor(time/ 60)
-        let minute = Math.round((time-hour) *60)
-
-        return `${hour} soat  ${minute} minut`
-    }
+function valution (vaqt){
+    let soat = Math.floor(vaqt)
+    let minut =Math.ceil((vaqt-soat)/60)
+    return `${soat} soat ${minut} minut`
+}
